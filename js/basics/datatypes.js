33f5e2f5083b460995d4555bed55f91a046f893f
Pooja <pooja.sharma7@cognizant.com>
2024-01-24 17:21:26 +0530

    datatypes

---
+++ js/basics/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// primitive data types
+
+// let name = "Pooja"; // string
+// let age = 24; // number
+// let isLoggedIn = false; // boolean
+// bigint
+// null
+// undefined
+// symbol => unique
+
+// object
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
