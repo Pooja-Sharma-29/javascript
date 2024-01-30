cac148aa14f7ff4484e19e13873b4a3c09b6622b
Pooja <pooja.sharma7@cognizant.com>
2024-01-30 14:06:51 +0530

    deleted

---
+++ js/basics/conversion.js
@@ -0,0 +1,20 @@
+let score = "33";
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNum = Number(score);
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+
+// when we convert "33" to 33 , it easily get converted
+// "33abc" => NaN (but when we check the type it gives number)
+// null => 0
+
+let isLoggedIn = 1;
+// console.log(Boolean(isLoggedIn));
+// console.log(Boolean("")); // false
+// console.log(Boolean(null)); // false
+// console.log(Boolean(undefined)); // false
+
+let num = 29;
+console.log(typeof String(num));
---
+++ js/basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+// primitive data types
+
+// let name = "Pooja"; // string
+// let age = 24; // number
+// let isLoggedIn = false; // boolean
+// bigint
+// null
+// undefined
+// symbol => unique
+
+// let bigNum = 49067823875659486n;
+// console.log(typeof bigNum); // bigint
+
+// object
+// console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+
+let a = 10;
+let b = a;
+b = 20;
+console.log(a, b);
